a10635455788b3e958d50d1ddb61d7e83a0dce0d
lucasramos97 <lucasramosdev@gmail.com>
2021-07-12 14:26:33 -0300

    Override duration field to return hour, minutes and seconds only

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,3 +1,11 @@
 class Music < ApplicationRecord
   validates_presence_of :title, :artist, :release_date, :duration
+
+  def duration
+    if not super
+      return nil
+    end
+
+    return super.strftime("%H:%M:%S")
+  end
 end
